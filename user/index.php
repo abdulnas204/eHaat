84d43e5476ce3781c809b51ea3c3d00471f59019
hippozippo <raj92.mckv@gmail.com>
2015-05-08 12:11:50 +0530

    added UI for user/profile.php

--- user/index.php
+++ user/index.php
@@ -1,14 +1,37 @@
 <?php
-require_once('./backend/precheck.php');
-require_once('../common/util.php');
+//<!--require_once('./backend/precheck.php');
+//require_once('../common/util.php');-->
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>eHaat - User</title>
+    <link rel="stylesheet" type="text/css" href="resource/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="resource/css/bootstrap-theme.min.css">
+    <script src="resource/js/jquery.min.js"></script>
+    <script type="text/javascript" src="resource/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<h1>This will be user's landing page</h1>
+	<div role="tabpanel">
+
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="home">...</div>
+    <div role="tabpanel" class="tab-pane" id="profile">...</div>
+    <div role="tabpanel" class="tab-pane" id="messages">...</div>
+    <div role="tabpanel" class="tab-pane" id="settings">...</div>
+  </div>
+
+</div>
 </body>
 </html>
--- user/profile.php
+++ user/profile.php
@@ -1,18 +1,21 @@
 <?php
-session_start();
-require_once('../common/util.php');
-require_once('./backend/precheck.php');
-require_once('backend/fetchUserInfo.php');
+//session_start();
+//require_once('../common/util.php');
+//require_once('./backend/precheck.php');
+//require_once('backend/fetchUserInfo.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>eHaat - User's order</title>
+	<link rel="stylesheet" href="../resource/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../resource/css/bootstrap-theme.min.css">
+    <script src="../resource/js/jquery.min.js"></script>
+    <script type="text/javascript" src="../resource/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<h1>My profile</h1>
-	<form action="./backend/updateProfile.php" method="POST">
+	<!--
 		<label for="username">Username</label>
 		<input name="username" type="text" value="<?=$username?>"><br>
 		<label for="email">Email ID</label>
@@ -21,5 +24,80 @@ require_once('backend/fetchUserInfo.php');
 		<input name="contact" type="number" value="<?=$contact?>"><br>
 		<input type="submit" name="update" value="Update">
 	</form>
+-->
+	<div class="container">
+			
+			<div id="header" class="container page-header text-center">
+				<div class="col-md-8">
+					<h1 class="voffset">Edit profile</h1>
+				</div>
+				<div class="col-md-2"></div>
+				<div class="col-md-2">
+					<!-- avatar-create new repo-logout -->
+					<div class="dropdown">
+		  
+					<a class="dropdown-toggle avatar" data-toggle="dropdown" role="button" aria-expanded="false" href="#">
+					    <img height="30" width="30" src="../resource/img/raj.jpg" alt="@hippozippo"></img>
+				        	<span>hippozippo</span>
+							<span class="caret"></span>
+					</a>
+			    	<ul class="dropdown-menu" role="menu">
+					    <li><a href="repo.php">Repositories</a></li>
+					    <li><a href="dashboard.php">Dashboard</a></li>
+					    <li><a href="../index.php">Logout</a></li>
+					</ul>
+					
+					</div>
+				</div>
+			</div>
+
+			<form action="./backend/updateProfile.php" method="POST">
+				<div class="form-group">
+
+					<label for="username" class="col-md-offset-1 col-sm-3 control-label">First Name</label>
+					<div class="col-sm-5">
+						<input type="text" name="username" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="username" class="col-md-offset-1 col-sm-3 control-label">Last name</label>
+					<div class="col-sm-5">
+						<input type="text" name="username" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="username" class="col-md-offset-1 col-sm-3 control-label">Username</label>
+					<div class="col-sm-5">
+						<input type="text" name="username" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="email" class="col-md-offset-1 col-sm-3 control-label">Email</label>
+					<div class="col-sm-5">
+						<input type="text" name="email" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="password" class="col-md-offset-1 col-sm-3 control-label">Old Password</label>
+					<div class="col-sm-5">
+						<input type="password" name="password" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="password" class="col-md-offset-1 col-sm-3 control-label">New Password</label>
+					<div class="col-sm-5">
+						<input type="password" name="password" class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="confirm_pass" class="col-md-offset-1 col-sm-3 control-label">Confirm new Password</label>
+					<div class="col-sm-5">
+						<input type="password" name="confirm_pass" class="form-control">
+					</div>
+				</div>
+				<div class="col-md-offset-5 col-md-3">
+					<input type="submit" name="register-btn" id="register-btn" class="btn btn-success btn-block voffset1" value="Update">
+				</div>
+			</form>
 </body>
 </html>
